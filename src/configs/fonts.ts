9a4503d68786526ee116b8e33c2fa0da7b92e531
Luciana Ribeiro <albuquerqueluciana54@gmail.com>
2022-11-28 19:29:00 -0300

    Add colors and font styles to project #26
    Co-authored-by: Luciana Albuquerque <albuquerqueluciana54@gmail.com>
    Co-authored-by: Larissa Sales <larissa.s.sales@outlook.com>

---
+++ src/configs/fonts.ts
@@ -0,0 +1,20 @@
+import {Colors} from './colors';
+
+export const FontSize = {
+    xx_small: 12,
+    x_small: 14,
+    small: 16,
+    medium: 20,
+    large: 20,
+    x_large: 22,
+    xx_large: 24,
+    xxx_large: 32
+}
+
+export const FontWeight = {
+    light: "300",
+    regular: "regular",
+    medium: "500",
+    semi_bold: "600",
+    bold: "700",
+}
