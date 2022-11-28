e29c0e0ee165b97dbb853f4608826ec8e8a78a77
Luciana Ribeiro <albuquerqueluciana54@gmail.com>
2022-11-28 19:29:00 -0300

    Add colors and font styles to project #26
    Co-authored-by: Luciana Albuquerque <albuquerqueluciana54@gmail.com>
    Co-authored-by: Larissa Sales <larissa.s.sales@outlook.com>

---
+++ src/configs/colors.ts
@@ -0,0 +1,12 @@
+export const Colors = {
+    white: '#FFFFFF',
+    dark: '#000000',
+    green_android: '#A7C957',
+    green_sap: '#4F772D',
+    green_hunder: '#31572C',
+    green_phthalo: '#132A13',
+    alburn: '#9E2A2B',
+    grey_light: '#F2F2F2',
+    grey: '#A0A0A0',
+    grey_medium_dark: '#8A8A8A'
+}
---
+++ src/configs/fontStyle.ts
@@ -0,0 +1,53 @@
+import { StyleSheet } from 'react-native';
+import { FontSize } from './fonts';
+
+export const fontStyles = StyleSheet.create({
+    default: {
+        fontSize: FontSize.small,
+        fontWeight: "400",
+        letterSpacing: 3,
+    },
+    
+    defaultSemibold: {
+        fontSize: FontSize.small,
+        fontWeight: "600",
+        letterSpacing: 3,
+    },
+    
+    modal: {
+        fontSize: 14,
+        fontWeight: "400",
+        letterSpacing: 3,
+        paragraphSpacing: 5,
+    },
+    
+    link: {
+        fontSize: FontSize.x_small,
+        fontWeight: "600",
+        letterSpacing: 3,
+    },
+    
+    header: {
+        fontSize: FontSize.xxx_large,
+        fontWeight: "700",
+        letterSpacing: 3,
+    },
+    
+    header2: {
+        fontSize: FontSize.large,
+        fontWeight: "600",
+        letterSpacing: 3,
+    },
+    
+    button: {
+        fontSize: FontSize.x_large,
+        fontWeight: "400",
+        letterSpacing: 3,
+    },
+      
+    subtitle: {
+        fontSize: FontSize.xx_small,
+        fontWeight: "400",
+        letterSpacing: 3,
+    },
+});
