ed1be9a05b6019870e51746e4f99619e8ab9c760
Luciana Ribeiro <albuquerqueluciana54@gmail.com>
2022-11-30 18:59:46 -0300

    Create styles for all button types #26
    Co-authored-by: Luciana Albuquerque <albuquerqueluciana54@gmail.com>
    Co-authored-by: Larissa Sales <larissa.s.sales@outlook.com>

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,14 +1,28 @@
 import React from "react";
 import { TouchableOpacity, Text } from "react-native";
-import { fontStyles } from "../../configs/fontStyle";
 import { styles } from "./styles";
+import { buttonTypes } from "../../constants/buttonsTypes";
 
 
-const Button = ({onPress, title, type = 'primary'}): React.ReactElement => {
-    const isPrimary = type === 'primary';
+const Button = ({onPress, title, type = buttonTypes.Primary}): React.ReactElement => {
+    const buttonType = {
+        [buttonTypes.Primary]: styles.primaryButton,
+        [buttonTypes.Secondary]: styles.secondaryButton,
+        [buttonTypes.SmallPrimary]: styles.primarySmallButton,
+        [buttonTypes.SmallSecondary]: styles.secondarySmallButton,
+        [buttonTypes.SmallDanger]: styles.dangerSmallButton
+    }
+    const textType = {
+        [buttonTypes.Primary]: styles.primaryText,
+        [buttonTypes.Secondary]: styles.secondaryText,
+        [buttonTypes.SmallPrimary]: styles.primarySmallText,
+        [buttonTypes.SmallSecondary]: styles.secondarySmallText,
+        [buttonTypes.SmallDanger]: styles.dangerSmallText
+    }
+
     return (
-        <TouchableOpacity onPress={onPress} style={isPrimary ? styles.primaryButton : styles.secondaryButton}>
-            <Text style={[fontStyles.button, isPrimary ? styles.primaryText : styles.secondaryText]}>
+        <TouchableOpacity onPress={onPress} style={buttonType[type]}>
+            <Text style={textType[type]}>
                 {title}
             </Text>
         </TouchableOpacity>
--- storybook/stories/Button/Button.stories.js
+++ storybook/stories/Button/Button.stories.js
@@ -1,16 +1,23 @@
-import { action } from '@storybook/addon-actions';
-import { text } from '@storybook/addon-knobs';
-import { storiesOf } from '@storybook/react-native';
 import React from 'react';
-import { Text } from 'react-native';
-import Button from '../../../src/components/Button/index';
 import CenterView from '../CenterView';
+import Button from '../../../src/components/Button/index';
+import { storiesOf } from '@storybook/react-native';
+import { buttonTypes } from '../../../src/constants/buttonsTypes';
 
 storiesOf('Button', module)
   .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
   .add('Primary Button', () => (
-    <Button onPress={() => {console.log('clicked')}} title="Primary" type="primary"/>
+    <Button onPress={() => {console.log('clicked')}} title="Primary" type={buttonTypes.Primary}/>
   ))
   .add('Secondary Button', () => (
-    <Button onPress={() => {console.log('clicked')}} title="Secondary" type="secondary"/>
+    <Button onPress={() => {console.log('clicked')}} title="Secondary" type={buttonTypes.Secondary}/>
+  ))
+  .add('Primary Small Button', () => (
+    <Button onPress={() => {console.log('clicked')}} title="Salvar" type={buttonTypes.SmallPrimary}/>
+  ))
+  .add('Secondary Small Button', () => (
+    <Button onPress={() => {console.log('clicked')}} title="Cancelar" type={buttonTypes.SmallSecondary}/>
+  ))
+  .add('Danger Small Button', () => (
+    <Button onPress={() => {console.log('clicked')}} title="Confirmar" type={buttonTypes.SmallDanger}/>
   ))
