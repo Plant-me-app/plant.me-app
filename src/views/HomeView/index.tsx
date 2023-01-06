5c7400d202f583cf1b8f86440f63945c5590c91c
Larissa Sales <larissa.s.sales@outlook.com>
2023-01-06 18:14:56 -0300

    Create empty home screen
    #10
    
    Co-authored-by: Larissa Sales <larissa.s.sales@outlook.com>
    Co-authored-by: Luciana Albuquerque <albuquerqueluciana54@gmail.com>

--- src/views/HomeView/index.tsx
+++ src/views/HomeView/index.tsx
@@ -1,14 +1,45 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Image, Text, View } from "react-native";
+import SearchBar from "../../components/SearchBar";
 import { Labels } from "../../constants/label.constants";
+import { getPlants } from "../../services/plant.service";
 import { styles } from "./styles";
 
-const HomeView = (): React.ReactElement => {
+const HomeView = ({ navigation }): React.ReactElement => {
+  const emptyScreen = require("../../images/HomeEmpty.png");
+  const arrowDown = require("../../images/ArrowDown.png");
+  const [plants, setPlants] = useState([]);
+
+  useEffect(() => {
+    const plants = async () => {
+      const result = await getPlants();
+      console.log("result", result.data.data);
+      setPlants(result.data.data);
+    };
+    const getsPlants = navigation.addListener("focus", () => {
+      plants();
+    });
+    plants();
+    return getsPlants;
+  }, [navigation]);
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerTitle}>{Labels.homeHeader}</Text>
       </View>
+      <View style={styles.searchContainer}>
+        <SearchBar />
+      </View>
+      {plants.length === 0 && (
+        <View style={styles.emptyBody}>
+          <Image source={emptyScreen} style={styles.emptyImage} />
+          <View style={styles.labelContainer}>
+            <Text style={styles.emptyLabel}>{Labels.emptyHomeLabel}</Text>
+            <Image source={arrowDown} style={styles.arrowIcon} />
+          </View>
+        </View>
+      )}
     </View>
   );
 };
--- src/views/HomeView/styles.ts
+++ src/views/HomeView/styles.ts
@@ -1,12 +1,31 @@
 import { StyleSheet } from "react-native";
 import { Colors } from "../../configs/colors";
-import { heightPercentageToDP as hp } from "react-native-responsive-screen";
+import { heightPercentageToDP as hp, widthPercentageToDP as wp } from "react-native-responsive-screen";
 import { fontStyles } from "../../configs/fontStyle";
 
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: Colors.white
+    },
+    
+    emptyBody: {
+        flex: 1, 
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+    },
+
+    searchContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
+
+    labelContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: "90%"
     },
 
     header: {
@@ -21,5 +40,24 @@ export const styles = StyleSheet.create({
         color: Colors.dark,
         paddingTop: hp("3%"),
         fontSize: hp("4%"),
+    },
+
+    emptyImage: {
+        width: hp('40%'),
+        height: hp('40%')
+    },
+
+    emptyLabel: {
+        ...fontStyles.defaultSemibold,
+        color: Colors.grey,
+        fontSize: hp('3%'),
+        width: wp('70%'),
+        textAlign: 'center'
+    },
+
+    arrowIcon: {
+        width: wp('7%'),
+        height: wp('7%'),
+        left: wp('2%')
     }
 })
