6766d9dd8a2c475885d95fba4ee0e0b2453bc711
“Luciana <albuquerqueluciana54@gmail.com>
2023-01-14 11:11:06 -0300

    Create plant details styles
    #9
    
    Co-authored-by: Luciana Albuquerque <albuquerqueluciana54@gmail.com>
    Co-authored-by: Larissa Sales <larissa.s.sales@outlook.com>

--- src/views/PlantDetails/index.tsx
+++ src/views/PlantDetails/index.tsx
@@ -1,8 +1,39 @@
 import React from "react";
-import { Text } from "react-native";
+import { Image, View, Text } from "react-native";
+import { styles } from "./styles";
 
+const editIcon = require("../../assets/images/Details/Edit.png");
+const infoIcon = require("../../assets/images/Details/Info.png");
+const level1Icon = require("../../assets/images/Details/Level1.png");
 const PlantDetails = (): React.ReactElement => {
-  return <Text>PlantDetails View</Text>;
+  const plant = {
+    name: "Juninha",
+    image: 21,
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <View style={styles.headerIcons}>
+          <View style={styles.iconContainer}>
+            <Image source={infoIcon} style={styles.detailsIcons} />
+            <Text style={styles.iconSubtitle}>Infos</Text>
+          </View>
+          <View style={styles.iconContainer}>
+            <Image source={level1Icon} style={styles.detailsIcons} />
+            <Text style={styles.iconSubtitle}>Nível</Text>
+          </View>
+          <View style={styles.iconContainer}>
+            <Image source={editIcon} style={styles.detailsIcons} />
+            <Text style={styles.iconSubtitle}>Editar</Text>
+          </View>
+        </View>
+        <Image source={plant.image} style={styles.plantImg} />
+        <Text style={styles.title}>{plant.name}</Text>
+      </View>
+      <View style={styles.body}></View>
+    </View>
+  );
 };
 
 export default PlantDetails;
