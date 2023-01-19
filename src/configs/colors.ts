dda35c034e12db1a2941c8620e3589552341bd6a
Larissa Sales <larissa.s.sales@outlook.com>
2023-01-19 18:36:49 -0300

    Create style to tasks
    #11
    
    Co-authored-by: Luciana Albuquerque <albuquerqueluciana54@gmail.com>
    Co-authored-by: Larissa Sales <larissa.s.sales@outlook.com>

--- src/configs/colors.ts
+++ src/configs/colors.ts
@@ -8,5 +8,9 @@ export const Colors = {
     alburn: '#9E2A2B',
     grey_light: '#F2F2F2',
     grey: '#A0A0A0',
-    grey_medium_dark: '#8A8A8A'
+    grey_medium_dark: '#8A8A8A',
+    water: '#00ADD3',
+    sun: '#EDD500',
+    soil: '#8F5F17',
+    fertilizer: '#D07523'
 }
--- src/views/PlantDetails/TaskModalContent/index.tsx
+++ src/views/PlantDetails/TaskModalContent/index.tsx
@@ -1,5 +1,7 @@
-import React from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Image, Text, TouchableOpacity, View } from "react-native";
+import { Colors } from "../../../configs/colors";
+import { TaskTypes } from "../../../constants/taskTypes.enum";
 import { styles } from "./styles";
 
 interface ITaskContent {
@@ -8,7 +10,13 @@ interface ITaskContent {
   onPress?: () => void;
   history: string;
   btnTitle: string;
+  taskType?: TaskTypes;
 }
+
+const waterIcon = require("../../../assets/images/Details/TasksModal/Water.png");
+const soilIcon = require("../../../assets/images/Details/TasksModal/Soil.png");
+const sunIcon = require("../../../assets/images/Details/TasksModal/Sun.png");
+const fertilizerIcon = require("../../../assets/images/Details/TasksModal/Fertilizer.png");
 
 const TaskModalContent = ({
   plant,
@@ -16,18 +24,55 @@ const TaskModalContent = ({
   onPress,
   history,
   btnTitle,
-}: ITaskContent): React.ReactElement => (
-  <View>
-    <View style={styles.header}>
-      <Text style={styles.headerTitle}>{headerTitle}</Text>
+  taskType,
+}: ITaskContent): React.ReactElement => {
+  const [currentTask, setCurrentTask] = useState(null);
+
+  const loadContent = () => {
+    const content = {
+      [TaskTypes.Water]: {
+        color: Colors.water,
+        icon: waterIcon,
+      },
+      [TaskTypes.Soil]: {
+        color: Colors.soil,
+        icon: soilIcon,
+      },
+      [TaskTypes.Light]: {
+        color: Colors.sun,
+        icon: sunIcon,
+      },
+      [TaskTypes.Fertilizer]: {
+        color: Colors.fertilizer,
+        icon: fertilizerIcon,
+      },
+    };
+    console.log("taskType", taskType);
+    const selectedTask = content[taskType];
+    setCurrentTask(selectedTask);
+  };
+
+  useEffect(() => {
+    loadContent();
+    console.log("content", currentTask);
+  }, [taskType]);
+
+  return (
+    <View>
+      <View style={styles.header}>
+        <Image source={waterIcon} style={styles.icon} />
+        <Text style={styles.headerTitle}>{headerTitle}</Text>
+      </View>
+      <View style={styles.body}>
+        <Text style={styles.history}>
+          Última {headerTitle}: {history}
+        </Text>
+        <TouchableOpacity onPress={onPress} style={styles.button}>
+          <Text style={styles.btnTitle}>{btnTitle}</Text>
+        </TouchableOpacity>
+      </View>
     </View>
-    <View style={styles.body}>
-      <Text style={styles.history}>Última Rega: {history}</Text>
-      <TouchableOpacity onPress={onPress} style={styles.button}>
-        <Text>{btnTitle}</Text>
-      </TouchableOpacity>
-    </View>
-  </View>
-);
+  );
+};
 
 export default TaskModalContent;
