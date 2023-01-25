cb0a111d1743a3390611d56da1ff5e626d948917
Luciana Ribeiro <albuquerqueluciana54@gmail.com>
2023-01-24 23:03:53 -0300

    Create edition plant feature
    #7
    
    Co-authored-by: Luciana Albuquerque <albuquerqueluciana54@gmail.com>
    Co-authored-by: Larissa Sales <larissa.s.sales@outlook.com>

--- src/services/plant.service.ts
+++ src/services/plant.service.ts
@@ -11,11 +11,20 @@ export const createPlant = async (plant: IPlant) => {
     });
 }
 
+export const updatePlant = async (plant: IPlant, id: string) => {
+    const result = await api.put(`/plants/${id}`, plant);
+    return new Promise((resolve, reject) => {
+         setTimeout(() => {
+             resolve(result);
+         }, 500);
+     });
+ }
+
 export const getPlants = async () => {
     return await api.get('/plants');
 }
 
-export const deletePlant = async (id: any) => {
+export const deletePlant = async (id: string) => {
     const result = await api.delete(`/plants/${id}`);
     return new Promise((resolve, reject) => {
         setTimeout(() => {
