066e89729ac039ee18065926d53388fe30a55fe5
Larissa Sales <larissa.s.sales@outlook.com>
2023-01-29 17:04:30 -0300

    Shows splashscreen only on app first launch
    #3
    
    Co-authored-by: Luciana Albuquerque <albuquerqueluciana54@gmail.com>
    Co-authored-by: Larissa Sales <larissa.s.sales@outlook.com>

--- src/routes/navigation/rootNavigator.tsx
+++ src/routes/navigation/rootNavigator.tsx
@@ -1,21 +1,39 @@
-import React from "react";
-import {NavigationContainer, StackActions} from '@react-navigation/native';
+import React, { useEffect, useState } from "react";
+import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import TabNavigator from "./TabNavigator";
 import SplashView from "../../views/SplashView";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Stack = createNativeStackNavigator();
 
 const RootNavigator = (): React.ReactElement => {
+	const [isAppFirstLaunch, setIsAppFirstLaunch] = useState(null);
 
-    return (
-        <NavigationContainer>
-            <Stack.Navigator screenOptions={{headerShown: false}}>
-                <Stack.Screen name="SplashScreen" component={SplashView} />
-                <Stack.Screen name="TabNavigator" component={TabNavigator} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    );
+	useEffect(() => {
+		const fetchData = async () => {
+			const appData = await AsyncStorage.getItem("isAppFirstLaunch");
+			if(appData == null) { 
+				setIsAppFirstLaunch(true);
+				AsyncStorage.setItem("isAppFirstLaunch", 'false');
+			}
+			else { 
+				setIsAppFirstLaunch(false)
+			}
+		}
+		fetchData().catch(console.error)
+	}, []);
+
+	return (
+		isAppFirstLaunch != null && (
+			<NavigationContainer>
+				<Stack.Navigator screenOptions={{ headerShown: false }}>
+					{isAppFirstLaunch && ( <Stack.Screen name="SplashScreen" component={SplashView} />)}
+					 <Stack.Screen name="TabNavigator" component={TabNavigator} />
+				</Stack.Navigator>
+			</NavigationContainer>
+		)
+	);
 };
 
 export default RootNavigator;
