7c0fbe2fc402346b90e1b6551bd097f733799eb8
Luciana Ribeiro <albuquerqueluciana54@gmail.com>
2023-02-01 19:16:23 -0300

    Fix splash screen button style
    Co-authored-by: Luciana Albuquerque <albuquerqueluciana54@gmail.com>
    Co-authored-by: Larissa Sales <larissa.s.sales@outlook.com>

--- src/views/SplashView/index.tsx
+++ src/views/SplashView/index.tsx
@@ -1,49 +1,50 @@
-import React from 'react';
-import { View, Text, Image, AsyncStorage,  } from 'react-native';
-import AppIntroSlider from 'react-native-app-intro-slider';
-import { styles } from './styles';
+import React from "react";
+import { View, Text, Image, AsyncStorage } from "react-native";
+import AppIntroSlider from "react-native-app-intro-slider";
+import { styles } from "./styles";
 import { Colors } from "../../configs/colors";
 import { Labels } from "../../constants/label.constants";
 
 const SplashView = ({ navigation }): React.ReactElement => {
+  const slides = [
+    {
+      key: "s1",
+      title: Labels.splashScreenTitle1,
+      text: Labels.splashScreenText1,
+      image: require("../../../src/assets/images/SplashScreen/splash1.png"),
+      backgroundColor: Colors.white,
+    },
+    {
+      key: "s2",
+      title: Labels.splashScreenTitle2,
+      text: Labels.splashScreenText2,
+      image: require("../../../src/assets/images/SplashScreen/splash2.png"),
+      backgroundColor: Colors.white,
+    },
+    {
+      key: "s3",
+      title: Labels.splashScreenTitle3,
+      text: Labels.splashScreenText3,
+      image: require("../../../src/assets/images/SplashScreen/splash3.png"),
+      backgroundColor: Colors.white,
+    },
+  ];
 
-	const slides = [{
-		key: 's1',
-		title: Labels.splashScreenTitle1,
-		text: Labels.splashScreenText1,
-		image: require('../../../src/assets/images/SplashScreen/splash1.png'),
-		backgroundColor: Colors.white,
-	},
-	{
-		key: 's2',
-		title: Labels.splashScreenTitle2,
-		text: Labels.splashScreenText2,
-		image: require('../../../src/assets/images/SplashScreen/splash2.png'),
-		backgroundColor: Colors.white,
-	},
-	{
-		key: 's3',
-		title: Labels.splashScreenTitle3,
-		text: Labels.splashScreenText3,
-		image: require('../../../src/assets/images/SplashScreen/splash3.png'),
-		backgroundColor: Colors.white,
-	}];
+  const onDone = () => {
+    navigation.replace("TabNavigator");
+  };
 
-	const onDone = () => {
-		navigation.replace("TabNavigator");
-  }
+  const renderItem = ({ item }) => {
+    return (
+      <View style={styles.splashContainer}>
+        <Image style={styles.img} source={item.image} />
+        <Text style={styles.title}>{item.title}</Text>
+        <Text style={styles.text}>{item.text}</Text>
+      </View>
+    );
+  };
 
-	const renderItem = ({ item }) => {
-		return (
-			<View style={styles.splashContainer}>
-				<Image style={styles.img} source={item.image} />
-				<Text style={styles.title}>{item.title}</Text>
-				<Text style={styles.text}>{item.text}</Text>
-			</View>
-		);
-	};
-
-	const renderNextButton = () => {
+  const renderNextButton = () => {
     return (
       <View>
         <Text style={styles.nextLink}>{Labels.splashScreenNext}</Text>
@@ -53,32 +54,32 @@ const SplashView = ({ navigation }): React.ReactElement => {
 
   const renderDoneButton = () => {
     return (
-			<View>
-				<Text style={styles.doneBtn}>{Labels.splashScreenDone}</Text>
+      <View style={styles.buttonWrapper}>
+        <View style={styles.doneBtnContainer}>
+          <Text style={styles.doneBtnText}>{Labels.splashScreenDone}</Text>
+        </View>
       </View>
     );
   };
 
-	const renderSkipButton = () => {
-    return (
-        <Text style={styles.skipLink}>{Labels.splashScreenSkip}</Text>
-    );
+  const renderSkipButton = () => {
+    return <Text style={styles.skipLink}>{Labels.splashScreenSkip}</Text>;
   };
 
-	return (
-		<AppIntroSlider
-			data={slides}
-			renderItem={renderItem}
-			onDone={onDone}
-			onSkip={onDone}
-			showSkipButton={true}
+  return (
+    <AppIntroSlider
+      data={slides}
+      renderItem={renderItem}
+      onDone={onDone}
+      onSkip={onDone}
+      showSkipButton={true}
       renderNextButton={renderNextButton}
-			renderDoneButton={renderDoneButton}
-			renderSkipButton={renderSkipButton}
-			activeDotStyle={styles.activeDotStyle}
-			dotStyle={styles.dotStyle}
-		/>
-	)
-}
+      renderDoneButton={renderDoneButton}
+      renderSkipButton={renderSkipButton}
+      activeDotStyle={styles.activeDotStyle}
+      dotStyle={styles.dotStyle}
+    />
+  );
+};
 
 export default SplashView;
