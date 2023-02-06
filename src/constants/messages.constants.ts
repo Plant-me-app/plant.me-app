557b43d4b57b9882b0e5e8b3a30e12c86b63653d
Luciana Ribeiro <albuquerqueluciana54@gmail.com>
2023-02-05 23:47:08 -0300

    Implements points to next level
    #22
    
    Co-authored-by: Larissa Sales <larissa.s.sales@outlook.com>
    Co-authored-by: Luciana Albuquerque <albuquerqueluciana54@gmail.com>

--- src/constants/messages.constants.ts
+++ src/constants/messages.constants.ts
@@ -3,5 +3,7 @@ export const Messages = {
     taskSuccess: "agora está mais forte na luta contra as pragas.",
     deleteMessage: "Tem certeza que deseja remover o registro da última",
     questionMark: "?",
-    noRecord: "Sem Registro"
+    noRecord: "Sem Registro",
+    levelMessage1: 'Pontos para o próximo nível:',
+    levelMessage2: 'Desbloqueia um novo avatar!'
 }
