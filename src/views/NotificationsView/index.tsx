5ac9a2586d5793d0c7f6ee8d538d7368939b1b02
Larissa Sales <larissa.s.sales@outlook.com>
2023-02-08 21:58:28 -0300

    Padronize all headers and create notifications screen style
    Co-authored-by: Luciana Albuquerque <albuquerqueluciana54@gmail.com>
    Co-authored-by: Larissa Sales <larissa.s.sales@outlook.com>

--- src/views/NotificationsView/index.tsx
+++ src/views/NotificationsView/index.tsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+import { styles } from "./styles";
 
 const NotificationView = (): React.ReactElement => {
-
-    return (
-        <Text>NotificationView</Text>
-    );
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Notificações</Text>
+      </View>
+      <View style={styles.body}></View>
+    </View>
+  );
 };
 
 export default NotificationView;
